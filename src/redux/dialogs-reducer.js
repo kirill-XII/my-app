db4f223119d2e3b5da4a0aa8e8e194a63d27c488
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-01-12 11:33:42 +0300

    switch, case

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,14 +2,22 @@ const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
 const dialogsReducer = (state, action) => {
-	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-		state.newMassageBody = action.body
-	} else if (action.type === SEND_MESSAGE) {
-		let body = state.newMassageBody
-		state.newMassageBody = ''
-		state.messages.push({ id: 6, message: body })
+	switch (action.type) {
+		case UPDATE_NEW_MESSAGE_BODY:
+			state.newMassageBody = action.body
+			return state
+		case SEND_MESSAGE:
+			let body = state.newMassageBody
+			state.newMassageBody = ''
+			state.messages.push({ id: 6, message: body })
+			return state
+		default:
+			return state
 	}
-
-	return state
 }
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE })
+export const updateNewMessageBodyCreator = body => ({
+	type: UPDATE_NEW_MESSAGE_BODY,
+	body: body,
+})
 export default dialogsReducer
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,18 +1,28 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const profileReducer = (state, action) => {
-	if (action.type === ADD_POST) {
-		let newPost = {
-			id: 5,
-			message: state.newPostText,
-			likesCount: 2,
-		}
-		state.posts.push(newPost)
-		state.newPostText = ''
-	} else if (action.type === UPDATE_NEW_POST_TEXT) {
-		state.newPostText = action.newText
-	}
+	switch (action.type) {
+		case ADD_POST:
+			let newPost = {
+				id: 5,
+				message: state.newPostText,
+				likesCount: 2,
+			}
 
-	return state
+			state.posts.push(newPost)
+			state.newPostText = ''
+			return state
+
+		case UPDATE_NEW_POST_TEXT:
+			state.newPostText = action.newText
+			return state
+		default:
+			return state
+	}
 }
+export const addPostActionCreator = () => ({ type: ADD_POST })
+export const updateNewPostActionCreator = text => ({
+	type: UPDATE_NEW_POST_TEXT,
+	newText: text,
+})
 export default profileReducer
